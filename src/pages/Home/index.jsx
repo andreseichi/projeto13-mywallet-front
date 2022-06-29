21155996ff9681f07726c0b0f222cc35f9b77673
andreseichi <andre_seichi@hotmail.com>
2022-06-29 15:20:13 -0300

    organização

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,11 @@
-import { FiLogOut } from 'react-icons/fi';
-
-// import { ThreeDots } from 'react-loader-spinner';
 import { Summary } from '../../components/Summary';
+import { TransactionInput } from '../../components/TransactionInput';
+
+import { FiLogOut } from 'react-icons/fi';
 
 import { Container, Header, Greeting, TransactionsInput } from './styles';
 
-import { TransactionInput } from '../../components/TransactionInput';
-
 export function Home() {
-  // const navigate = useNavigate();
-
   return (
     <Container>
       <Header>
